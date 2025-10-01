563f81ccf6f1b407ffd4525707b6e0b078d41804
Ubuntu <ubuntu@ip-172-31-22-144.ec2.internal>
2025-10-01 07:43:45 +0000

    Added automated build test message

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,6 +1,7 @@
 public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins! My first build is successful.");
+	System.out.println("This is an automated build test after modifying the file.");
     }
 }
 
